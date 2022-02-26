d6cffac7a71e7c2450dcd8dafd708698d06463b6
AndreiOmnom <belcargoru@gmail.com>
2022-02-26 15:45:53 +0300

    Задание 5-8 от 26.02 суббота

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import com.sun.jdi.IntegerValue;
+
 public class Main {
     public static void main(String[] args) {
-         int q = 12;
-         q++;
-         q--;
-         System.out.println(q);
+         Integer max  = Integer.MAX_VALUE;
+        Integer min  = Integer.MIN_VALUE;
+        Byte max1 = Byte.MAX_VALUE;
+        Byte min1 = Byte.MIN_VALUE;
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(max1);
+        System.out.println(min1);
+
 
     }
 }
